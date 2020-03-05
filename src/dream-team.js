96d65edb57451ab1c1291171a56bcb4ef8b54274
Ihar Trymailau <ihar.trymailau@gmail.com>
2020-03-05 17:45:55 +0300

    feat: add dream-ream, transform-array

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let myTeam = '';
+  
+  if(Array.isArray(members) == false) return false;
+  
+  for(let teamate of members.sort()) {
+    if(typeof teamate === 'string') {
+        myTeam += teamate.trim()[0];
+    }
+  }
+    
+  return myTeam ? myTeam.toUpperCase().split('').sort().join('') : false;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(Array.isArray(arr) == false) throw Error;
+
+    let newArr = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+            case "--double-next":
+                if(i < arr.length - 1) newArr.push(arr[i + 1]);
+                break;
+
+            case "--double-prev":
+                if(i) newArr.push(arr[i - 1]);
+                break;
+
+            case "--discard-next":
+                i++;
+                break;
+
+            case "--discard-prev": 
+                if(newArr.length) newArr.pop();
+                break;
+
+            default:
+                newArr.push(arr[i]);     
+        }
+    }
+    return newArr;      
 };
